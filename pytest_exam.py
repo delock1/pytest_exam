294de69c7e44d3c9814b54c3b86a56d28c2b942d
delock1 <smetanin.am@hotmail.com>
2021-03-02 00:26:50 +0300

    Update pytest_exam.py

--- pytest_exam.py
+++ pytest_exam.py
@@ -1,1 +1,22 @@
-import sys
+import pytest
+def func_float(x,y):
+    if x==0:
+        return x
+    return x/y
+def func_int(x):
+    return x*x
+@pytest.mark.parametrize("test_input,expected", [(-900, True), (900, True), (0.05, False)])
+def test_int1(test_input, expected):
+    assert isinstance(test_input,int) == expected
+def test_int2():
+    try:
+        assert func_int("string")
+    except TypeError:
+        pass
+def test_float():
+    assert isinstance(func_float(5,8),float) == True
+    assert isinstance(func_float(-1,7),float) == True
+    try:
+        assert isinstance(func_float("string", 1), float)
+    except TypeError:
+        pass
